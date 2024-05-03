7e26701b80e2b7dced33d8ebb544572988af9c28
*pknowledge* <mishrasangeeta944@gmail.com>
2024-05-03 11:11:23 +0530

    complete fronted

---
+++ fronted/src/main.jsx
@@ -0,0 +1,8 @@
+export default App;
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+);
